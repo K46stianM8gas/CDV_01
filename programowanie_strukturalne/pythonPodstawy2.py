5748e8cf6f2212e3313c60ed59927b653cb1e2ef
K46stianM8gas <k.migas600@gmail.com>
2019-03-23 14:31:02 +0100

    commit4

---
+++ programowanie_strukturalne/pythonPodstawy2.py
@@ -0,0 +1,20 @@
+#pi
+
+import math
+pi = math.pi
+print(pi)
+
+#pierwiastek
+pierwiastek = math.sqrt(9)
+print(pierwiastek)
+
+#moduł random
+
+import random
+losuj = random.random()
+print(losuj)
+
+#losuj z listy
+
+losujZlisty = random.choice("asdyhbfhasbdfkasd")
+print(losujZlisty)
