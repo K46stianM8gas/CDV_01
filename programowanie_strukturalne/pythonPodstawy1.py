defbfd913e20067d2ea035d3f03741adb006bb15
K46stianM8gas <k.migas600@gmail.com>
2019-03-23 14:26:06 +0100

    commit4

--- programowanie_strukturalne/pythonPodstawy1.py
+++ programowanie_strukturalne/pythonPodstawy1.py
@@ -1,35 +1,90 @@
 #podstawy
-print("CDV eloeloeloelo")
+#print("CDV eloeloeloelo")
 
 #potega
 
-potega = 2 ** 10
-print(potega)
+#potega = 2 ** 10
+#print(potega)
 
 
 # wypisanie dwa razy tekstu
-tekst = "CDV"
-print(tekst * 2)
+#tekst = "CDV"
+#print(tekst * 2)
 
 
 # pobieranie danych z  + konkatenacja stringów
 
-print("PODAJ IMIE GRZESZNIKU")
-imie = input()
-print ("Twoje imie podane z klawiatury " + imie)
+#print("PODAJ IMIE GRZESZNIKU")
+#imie = input()
+#print ("Twoje imie podane z klawiatury " + imie)
 
-nazwisko = "Nowak"
+#nazwisko = "Nowak"
 
-print("TWOJE DANE GRZESZNIKU: " + imie + " " + nazwisko)
+#print("TWOJE DANE GRZESZNIKU: " + imie + " " + nazwisko)
 
-dlugosc = len(nazwisko)
-print("DLUGOSC TWOJEGO NAZWISKA GRZESZNIKU = ")
-print(dlugosc)
+#dlugosc = len(nazwisko)
+#print("DLUGOSC TWOJEGO NAZWISKA GRZESZNIKU = ")
+#print(dlugosc)
 
-print(type(imie))
-print(type(nazwisko))
+#print(type(imie))
+#print(type(nazwisko))
 
-wiek = input()
-print("TWOJ WIEK: " + wiek)
-print(type(wiek))
+#wiek = input()
+#print("TWOJ WIEK: " + wiek)
+#print(type(wiek))
+
+tekst = "Paweł, Anna, Michał"
+
+tab = tekst.split(",")
+print(tekst)
+print(tab)
+
+imie1 = tab[0]
+print("Pierwsze imie w tablicy "+ imie1)
+
+imiemale = imie1.lower()
+print(imiemale)
+
+zawartosc = imie1.isalpha()
+print(zawartosc)
+
+imie2 = ""
+zawartosc2 = imie2.isalpha()
+print(zawartosc2)
+
+imie = "Julia"
+print("\n dane użytkownika \n Masz na imie:" + imie)
+
+
+text1 = "JAN\n"
+text2 = "NOWAK"
+print(text1 + text2)
+
+text1 = text1.rstrip()
+print(text1 + text2)
+
+
+#wyświetlanie łańcuchów znaków
+
+#wszystkie wersje pythona
+text = "%s, Java i %s" % ("PHP", "Python")
+print(text)
+
+#nowsze wersje
+text3 = "{0}, Java i {1}".format("PHP", "Python")
+print(text3)
+
+
+#help(text.replace)
+
+new = text.replace("PHP", "C#")
+print(new)
+
+#wypisanie danych
+
+dzien = 23
+miesiac = 3
+rok = 2019
+print("DATA: ", end="")
+print(dzien, miesiac, rok)
 
