a8733f696061dbff61f99ecabdff01e22bbff18e
K46stianM8gas <k.migas600@gmail.com>
2019-03-23 13:31:48 +0100

    commit3

--- programowanie_strukturalne/pythonPodstawy1.py
+++ programowanie_strukturalne/pythonPodstawy1.py
@@ -1,2 +1,35 @@
 #podstawy
 print("CDV eloeloeloelo")
+
+#potega
+
+potega = 2 ** 10
+print(potega)
+
+
+# wypisanie dwa razy tekstu
+tekst = "CDV"
+print(tekst * 2)
+
+
+# pobieranie danych z  + konkatenacja stringów
+
+print("PODAJ IMIE GRZESZNIKU")
+imie = input()
+print ("Twoje imie podane z klawiatury " + imie)
+
+nazwisko = "Nowak"
+
+print("TWOJE DANE GRZESZNIKU: " + imie + " " + nazwisko)
+
+dlugosc = len(nazwisko)
+print("DLUGOSC TWOJEGO NAZWISKA GRZESZNIKU = ")
+print(dlugosc)
+
+print(type(imie))
+print(type(nazwisko))
+
+wiek = input()
+print("TWOJ WIEK: " + wiek)
+print(type(wiek))
+
