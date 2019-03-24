a43b802361f86fc6fe42c39c1d660a7c634b1d8a
K46stianM8gas <k.migas600@gmail.com>
2019-03-24 14:41:22 +0100

    commit

---
+++ programowanie_strukturalne/warunki.py
@@ -0,0 +1,63 @@
+#instrukcje warunkowe
+
+
+x = 6
+if x == 5:
+    print("x jest równe ", x)
+else:
+    print("x jest różne od 5")
+
+#########################################
+
+y = True
+
+if y:
+    print("prawda")
+else:
+    print("fałsz")
+
+
+#############################################
+
+
+z = 5 > 6
+if z:
+    print("prawda")
+else:
+    print("fałsz")
+print(z)
+##########################################
+
+j = False
+if bool(j):
+    print("prawda", j)
+else:
+    print("Fałsz", j)
+
+#########################################
+
+if True:
+    print("PRAWDA")
+else:
+    print("FAŁSZ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
